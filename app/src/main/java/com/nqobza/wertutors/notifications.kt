aeb4965f00dc46ddd0fb3d4f5b2e76181fea527e
NQOBILE_08 <st10166349@vcconnect.edu.za>
2024-09-11 00:54:08 +0200

    Commit Message

--- app/src/main/java/com/nqobza/wertutors/notifications.kt
+++ app/src/main/java/com/nqobza/wertutors/notifications.kt
@@ -13,7 +13,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ListView
 import android.widget.Toast
 import com.nqobza.wertutors.R
 import com.nqobza.wetutors.MainActivity
