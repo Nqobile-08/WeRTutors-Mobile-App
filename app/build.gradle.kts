c7e354335592b0a78839c5595fa9153f1d7f2e5c
NQOBILE_08 <st10166349@vcconnect.edu.za>
2024-09-11 00:54:08 +0200

    Commit Message

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.storage.CacheResetOnProcessCanceled.enabled  // Deprecated feature, not required for enabling viewBinding
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -18,12 +16,12 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
     buildFeatures {
-        viewBinding = true  // This is the correct way to enable viewBinding, no need for the 'enabled' field below
+        viewBinding = true
     }
-
-
+    buildFeatures {
+        viewBinding = true
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -33,12 +31,10 @@ android {
             )
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-
     kotlinOptions {
         jvmTarget = "1.8"
     }
@@ -49,15 +45,13 @@ dependencies {
     implementation ("androidx.recyclerview:recyclerview:1.2.1")
     implementation ("com.google.android.material:material:1.5.0")
     implementation ("androidx.constraintlayout:constraintlayout:2.1.3")
-
-    implementation ("com.google.android.material:material:1.10.0")  // Duplication: 'material:1.5.0' and 'material:1.10.0' both included. Keep only the latest version.
-
+    implementation ("androidx.cardview:cardview:1.0.0")
+implementation("implementation 'com.google.android.material:material:1.2.1'")
+    implementation ("com.google.android.material:material:1.10.0")
     implementation ("androidx.drawerlayout:drawerlayout:1.2.0")
     implementation ("androidx.appcompat:appcompat:1.6.1")
     implementation ("androidx.fragment:fragment-ktx:1.8.2")
     implementation("androidx.core:core-splashscreen:1.0.1")
-
-    // The following use of 'libs' is correct and will link to versions from your version catalog (libs.versions.toml):
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -68,7 +62,6 @@ dependencies {
     implementation(libs.firebase.storage.ktx)
     implementation(libs.firebase.firestore.ktx)
     implementation(libs.androidx.preference)
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
