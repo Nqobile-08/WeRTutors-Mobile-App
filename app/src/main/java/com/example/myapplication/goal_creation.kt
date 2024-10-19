4d1cc78ae7a3a6876ef94f3be0284b45712d5a6e
Ratjatji Malatji <ST10088007@vcconnect.edu.za>
2024-10-19 19:31:35 +0200

    IC files

---
+++ app/src/main/java/com/example/myapplication/goal_creation.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class goal_creation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_goal_creation)
+    }
+}
