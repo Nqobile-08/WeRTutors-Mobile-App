8f9fb8bf85c593f49b0e1e661030f8314311e82b
Ratjatji Malatji <104779244+ratjatjim@users.noreply.github.com>
2024-10-20 19:58:17 +0200

    fiexed icons, app icon, to add review chips

--- app/src/main/java/com/ratjatji/eskhathinitutors/Tutors/MainActivity.kt
+++ app/src/main/java/com/ratjatji/eskhathinitutors/Tutors/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) // change to admin
 
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
